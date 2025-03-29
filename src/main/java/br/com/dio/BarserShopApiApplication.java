b16ca7e71dfbb3ffc72735083fe5efff477bb746
Rob Nascimento <zuranmrs2@gmail.com>
2025-03-29 16:16:32 -0300

    ajustando applications

---
+++ src/main/java/br/com/dio/BarserShopApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarserShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarserShopApiApplication.class, args);
+	}
+
+}
